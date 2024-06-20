fea5f0790aefe4d61410192e60bdfa96e3cae0a0
Karwai Pun <karypun@gmail.com>
2024-06-20 14:29:03 +0100

    Remove non-existent components

--- apps/docs/src/common/pages/components.tsx
+++ apps/docs/src/common/pages/components.tsx
@@ -8,62 +8,17 @@ const reduceToLookup = (acc: object, cur) => ({...acc, [cur.default.title]: cur}
 const buildLookup = v => v.reduce(reduceToLookup, {});
 
 const mainStories = [
-  require('../../../../../components/back-link/spec/BackLink.stories.mdx'),
-  require('../../../../../components/breadcrumbs/spec/Breadcrumbs.stories.mdx'),
-  require('../../../../../components/button/spec/Button.stories.mdx'),
-  require('../../../../../components/cookie-banner/spec/CookieBanner.stories.mdx'),
-  require('../../../../../components/checkboxes/spec/Checkboxes.stories.mdx'),
-  require('../../../../../components/date-input/spec/DateInput.stories.mdx'),
-  require('../../../../../components/details/spec/Details.stories.mdx'),
-  require('../../../../../components/error-message/spec/ErrorMessage.stories.mdx'),
-  require('../../../../../components/error-summary/spec/ErrorSummary.stories.mdx'),
-  require('../../../../../components/fieldset/spec/FieldSet.stories.mdx'),
-  require('../../../../../components/file-upload/spec/FileUpload.stories.mdx'),
-  require('../../../../../components/footer/spec/Footer.stories.mdx'),
-  require('../../../../../components/header/spec/Header.stories.mdx'),
-  require('../../../../../components/inset-text/spec/InsetText.stories.mdx'),
-  require('../../../../../components/notification-banner/spec/NotificationBanner.stories.mdx'),
-  require('../../../../../components/pagination/spec/Pagination.stories.mdx'),
-  require('../../../../../components/panel/spec/Panel.stories.mdx'),
-  require('../../../../../components/phase-banner/spec/PhaseBanner.stories.mdx'),
-  require('../../../../../components/radios/spec/Radios.stories.mdx'),
-  require('../../../../../components/select/spec/Select.stories.mdx'),
-  require('../../../../../components/skip-link/spec/SkipLink.stories.mdx'),
-  require('../../../../../components/summary-list/spec/SummaryList.stories.mdx'),
-  require('../../../../../components/table/spec/Table.stories.mdx'),
-  require('../../../../../components/tabs/spec/Tabs.stories.mdx'),
-  require('../../../../../components/tag/spec/Tag.stories.mdx'),
-  require('../../../../../components/text-input/spec/TextInput.stories.mdx'),
-  require('../../../../../components/textarea/spec/Textarea.stories.mdx'),
-  require('../../../../../components/warning-text/spec/WarningText.stories.mdx')
-];
-const unofficialStories = [
-  require('../../../../../components/aside/spec/Aside.stories.mdx'),
-  require('../../../../../components/form/spec/Form.stories.mdx'),
-  require('../../../../../components/form-field/spec/FormField.stories.mdx'),
-  require('../../../../../components/navigation-menu/spec/NavigationMenu.stories.mdx'),
-  require('../../../../../components/page/spec/Page.stories.mdx')
-];
-const internalStories = [
-  require('../../../../../components/button-group/spec/ButtonGroup.stories.mdx'),
-  require('../../../../../components/form-group/spec/FormGroup.stories.mdx'),
-  require('../../../../../components/hint/spec/Hint.stories.mdx'),
-  require('../../../../../components/input/spec/Input.stories.mdx'),
-  require('../../../../../components/label/spec/Label.stories.mdx'),
-  require('../../../../../components/link/spec/Link.stories.mdx'),
-  require('../../../../../components/summary-card/spec/SummaryCard.stories.mdx'),
-  require('../../../../../components/visually-hidden/spec/VisuallyHidden.stories.mdx'),
-  require('../../../../../components/width-container/spec/WidthContainer.stories.mdx')
+  //require('../../../../../components/warning-text/spec/WarningText.stories.mdx')
 ];
 
 const mainComponents = buildLookup(mainStories);
-const unofficialComponents = buildLookup(unofficialStories);
-const internalComponents = buildLookup(internalStories);
+//const unofficialComponents = buildLookup(unofficialStories);
+//const internalComponents = buildLookup(internalStories);
 
 const subpages = {
   ...mainComponents,
-  ...unofficialComponents,
-  ...internalComponents
+//  ...unofficialComponents,
+//  ...internalComponents
 };
 
 const nameParam = 'name';
