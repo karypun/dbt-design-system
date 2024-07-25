9259d01d30848d4b2bf771ff374adc7d905f2a5a
Karwai Pun <karypun@gmail.com>
2024-07-25 09:11:36 +0100

    test

--- apps/docs/src/common/pages/content.tsx
+++ apps/docs/src/common/pages/content.tsx
@@ -27,7 +27,7 @@ const Page: FC<PageProps> = ({ location }) => (
         <p>When a product is altered or processed to change what it was - for example,  being forged, coated or machined.</p>
 
  <p>Suggested help text for users: We need to know if the product will undergo any processing or alteration work, for example being forged, coated or machined.</p>
-
+<h3 id="altered">test</h3>
         <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
         <h2 className="a-z-header" id="b">B</h2>
