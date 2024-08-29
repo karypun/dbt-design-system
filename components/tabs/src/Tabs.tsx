f8ca3dcbb8315f72015193bd247d35e811677fa9
Karwai Pun <karypun@gmail.com>
2024-08-29 08:20:25 +0100

    updates

--- components/tabs/src/Tabs.tsx
+++ components/tabs/src/Tabs.tsx
@@ -1,30 +1,22 @@
 import { FC, createElement as h } from 'react';
 import { StandardProps, classBuilder } from '@not-govuk/component-helpers';
-import { Tabs as NGUTabs } from '@not-govuk/tabs';
+import { NavigationMenu, NavigationMenuProps } from '@not-govuk/navigation-menu';
 
 import '../assets/Tabs.scss';
 
-export type TabsProps = StandardProps & {
-  /** Description for the 'heading' prop */
-  heading?: string
+export type TabsProps = StandardProps & NavigationMenuProps & {
 };
 
 export const Tabs: FC<TabsProps> = ({
-  children,
   classBlock,
   classModifiers,
   className,
-  heading,
   ...attrs
 }) => {
   const classes = classBuilder('dbt-design-system-tabs', classBlock, classModifiers, className);
-  const title = heading || 'Tabs';
 
   return (
-    <div {...attrs} className={classes()}>
-      <h1 className={classes('heading')}>{title}</h1>
-      {children}
-    </div>
+    <NavigationMenu classModifiers="horizontal" {...attrs} className={classes()} />
   );
 };
 
