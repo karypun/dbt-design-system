bff94254a07781ead57b4d288b327ff4abe51f34
Karwai Pun <karypun@gmail.com>
2024-11-26 13:09:54 +0000

    update with frontend link

--- apps/docs/src/common/pages/get-started.tsx
+++ apps/docs/src/common/pages/get-started.tsx
@@ -45,7 +45,7 @@ const Page: FC<PageProps> = ({ location }) => (
     </div>
     <div className="govuk-grid-column-three-quarters">
       <h1>{title}</h1>
-        <p>This design system helps us understand what Regulatory Services look like and how they should behave. It builds on the <A href="https://design-system.service.gov.uk/">GOV.UK design system</A>.</p>
+        <p>This design system helps us understand what Trade and Regulatory Services look like and how they should behave. It builds on the <A href="https://design-system.service.gov.uk/">GOV.UK design system</A>.</p>
 
         <h2>Design assets</h2>
         <p>You can explore ideas and demonstrate page flows using these design assets:</p>
@@ -53,6 +53,14 @@ const Page: FC<PageProps> = ({ location }) => (
           <li><A href="https://internal.prototypes.uktrade.digital/lite-work">Licensing prototype kit (GOV.UK prototype kit)</A></li>
           <li><A href="https://app.mural.co/t/departmentforbusinessandtrad1574/m/departmentforbusinessandtrad1574/1718094413612/699e6b95d41d31ea4da1717eb1f645242da71026?sender=uf6ad2fb3e168bde1c2295660">Service blueprint template (Mural)</A></li>
 
+        </ul>
+
+        <h2>Development assets</h2>
+        <p>LITE frontend contains the code you need to start building a user interface for LITE's services. 
+          This includes both public facing transactional services and internal case management workflows.
+        </p>
+        <ul>
+          <li><A href="https://github.com/uktrade/lite-frontend">LITE frontend</A></li>
         </ul>
 
     </div>
